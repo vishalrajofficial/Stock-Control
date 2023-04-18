baee3d505a306f97519894b94ed6d1054b5ae088
Vishal Raj <9031842908.vk@gmail.com>
2023-04-18 13:02:55 +0530

    reverted back

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://stock-control-aaum.onrender.com"],
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
     credentials: true,
   })
 );
